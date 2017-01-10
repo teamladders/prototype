5b44ebc1357799e7763d1696d955a60e697b406d
Igor Tulmentyev <igtulm@gmail.com>
2017-01-10 23:38:02 +0400

    mail send on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use DB;
+use Mail;
 use App\User;
+use App\Mail\EmailVerification;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -66,6 +70,44 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'email_token' => str_random(10),
         ]);
+    }
+
+    /**
+    *  Over-ridden the register method from the "RegistersUsers" trait
+    *  Remember to take care while upgrading laravel
+    */
+    public function register(Request $request)
+    {
+        // Laravel validation
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            $this->throwValidationException($request, $validator);
+        }
+
+        // Using database transactions is useful here because stuff happening is actually a transaction
+        // I don't know what I said in the last line! Weird!
+        DB::beginTransaction();
+        try {
+            $user = $this->create($request->all());
+            // After creating the user send an email with the random token generated in the create method above
+            $email = new EmailVerification(new User(['email_token' => $user->email_token]));
+            Mail::to($user->email)->send($email);
+            DB::commit();
+            return back();
+        } catch(Exception $e) {
+            DB::rollback();
+            return back();
+        }
+    }
+
+
+    public function verify($token)
+    {
+        // The verified method has been added to the user model and chained here
+        // for better readability
+        User::where('email_token',$token)->firstOrFail()->verified();
+        return redirect('login');
     }
 }
