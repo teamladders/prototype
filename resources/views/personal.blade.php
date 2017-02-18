bc3738a1969aab4cd3bf6280cb278cfa86725a60
Alexander Kozlov <alexskozlov@gmail.com>
2017-02-18 18:46:58 +0400

    Fixed personal menu

--- resources/views/personal.blade.php
+++ resources/views/personal.blade.php
@@ -5,9 +5,9 @@
     <div class="row">
         <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked personal-menu">
-                <li role="presentation" class={{ Request::is('personal.dashboard') ? 'active' : '' }}><a href="{{ route('personal.dashboard') }}">Dashboard</a></li>
-                <li role="presentation" class={{ Request::is('personal.profile') ? 'active' : '' }}><a href="{{ route('personal.profile') }}">Profile</a></li>
-                <li role="presentation" class={{ Request::is('personal.team') ? 'active' : '' }}><a href="{{ route('personal.team') }}">Team</a></li>
+                <li role="presentation" class={{ Route::is('personal.dashboard') ? 'active' : '' }}><a href="{{ route('personal.dashboard') }}">Dashboard</a></li>
+                <li role="presentation" class={{ Route::is('personal.profile') ? 'active' : '' }}><a href="{{ route('personal.profile') }}">Profile</a></li>
+                <li role="presentation" class={{ Route::is('personal.team') ? 'active' : '' }}><a href="{{ route('personal.team') }}">Team</a></li>
             </ul>
         </div>
         <div class="col-md-9">
